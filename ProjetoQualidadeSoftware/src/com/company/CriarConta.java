89e289ffbfe98d1cbc6d3128821785b4e54b0a20
pedro <71900861@aluno.faculdadecotemig.br>
2021-08-31 18:27:49 -0300

    Criação do projeto
    Criação da classe CriarConta

---
+++ ProjetoQualidadeSoftware/src/com/company/CriarConta.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.lang.reflect.Constructor;
+
+public class CriarConta {
+
+    private int numeroConta;
+    private double saldo;
+    private String nome;
+    public CriarConta(int numeroConta, double saldo, String nome)
+    {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.nome = nome;
+    }
+}
